ff496e483442c538178becdf14da2e3c5f0e5843
AntonJoinToIT <nemermel18@gmail.com>
2017-05-26 11:30:05 +0300

    Working stage of remote upload script

--- upload.bash
+++ upload.bash
@@ -1,16 +1,24 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-cd /srv/http/$BASH_ARGV/
+SCRIPT="rm -rf /tmp/awesome.tar.gz && mv ~/awesome.tar.gz /tmp && cd /tmp && tar xvf awesome.tar.gz && cd /var/www/file_hosting_dev/public/ && rm -rf /var/www/file_hosting_dev/public/js && rm -rf /var/www/file_hosting_dev/public/css && mv /tmp/public/js ./js && mv /tmp/public/css ./css && exit"
+
+cd /var/www/html/$1/
+
 echo "Archiving public folder"
-tar -cf public.tar.gz ./public
+tar -cf awesome.tar.gz ./$2
+
 echo "public.tar.gz has been created"
-mv public.tar.gz /tmp
+mv awesome.tar.gz /tmp
+
 echo "Moving to tmp folder"
 cd /tmp
+
 echo "Sending to remote server"
-#scp /tmp/public.tar.gz developer_kovalenko@107.
+scp /tmp/awesome.tar.gz developer_kovalenko@107.170.18.92:~/
+
 echo "Remove /tmp copy of file"
-rm ./public.tar.gz
-#ssh developer_kovalenko@107.
-ls
+rm ./awesome.tar.gz
+ssh developer_kovalenko@107.170.18.92 ${SCRIPT}
+
+
 exit
